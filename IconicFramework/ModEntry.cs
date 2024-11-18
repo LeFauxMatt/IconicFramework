8a88d14da63f350dafb4ae66778bd3d5eba5e5f4
Matthew <3005344+LeFauxMatt@users.noreply.github.com>
2024-11-18 15:13:10 -0800

    Initial commit

--- IconicFramework/ModEntry.cs
+++ IconicFramework/ModEntry.cs
@@ -1,5 +1,15 @@
 namespace LeFauxMods.IconicFramework;
 
-public class ModEntry
+using LeFauxMods.IconicFramework.Utilities;
+
+/// <inheritdoc/>
+internal sealed class ModEntry : Mod
 {
+    /// <inheritdoc/>
+    public override void Entry(IModHelper helper)
+    {
+        // Init
+        I18n.Init(this.Helper.Translation);
+        _ = new Log(this.Monitor);
+    }
 }
